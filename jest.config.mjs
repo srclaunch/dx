63b1bfb5bb0328d5757864ff2e786ce2664f749d
Steven Bennett <steven@budgetbloom.com>
2022-04-21 14:04:43 -0700

    Quick release - patch

--- jest.config.mjs
+++ jest.config.mjs
@@ -11,5 +11,4 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ["**/src/tests/**/*.[jt]s?(x)", "**/src/__tests__/**/*.[jt]s?(x)", "**/src/?(*.)+(spec|test).[jt]s?(x)" ],
-  testPathIgnorePatterns: [],
 };
--- jest.config.umd.js
+++ jest.config.umd.js
@@ -1,16 +1,14 @@
 module.exports = {
   collectCoverageFrom: [
-    'src/*.{js,ts,tsx}',
-    'src/**/*.{js,ts,tsx}',
+    '**/src/*.{js,ts,tsx}',
+    '**/src/**/*.{js,ts,tsx}',
     '!**/src/__tests__/**',
     '!**/src/tests/**',
-    '!dist/**',
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   coverageReporters: ['json'],
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/cypress/'],
+  testMatch: ["**/src/tests/**/*.[jt]s?(x)", "**/src/__tests__/**/*.[jt]s?(x)", "**/src/?(*.)+(spec|test).[jt]s?(x)" ],
 };
