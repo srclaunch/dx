68b9774a2795039a01f9c94e966a6ac9345ef19b
Steven Bennett <steven@budgetbloom.com>
2022-04-21 07:06:08 -0700

    Quick release - patch

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,6 +8,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: [ "src/tests/*.[jt]s?(x)", "src/tests/**/*.[jt]s?(x)", "src/__tests__/**/*.[jt]s?(x)", "src/?(*.)+(spec|test).[jt]s?(x)" ],
+  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/cypress/'],
 };
