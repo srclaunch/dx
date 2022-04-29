b1e8deaa4af8c547a0264704d45fed6db30c9fac
Steven Bennett <steven@budgetbloom.com>
2022-04-29 15:55:29 -0700

    Quick release - patch

--- .srclaunch/config.js
+++ .srclaunch/config.js
@@ -1,9 +1,6 @@
-import {
-  ProjectType,
-} from '@srclaunch/types';
-
 export default {
-  name: '@srclaunch/dx',
-  description: 'Quality of life configuration and tooling for development environments.',
-  type: ProjectType.Library,
+  name: "@srclaunch/dx",
+  description:
+    "Quality of life configuration and tooling for development environments.",
+  type: "library",
 };
