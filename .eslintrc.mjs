8c1267dab09a2597286729b6425d08c073dc0350
Steven Bennett <steven@budgetbloom.com>
2022-05-04 15:50:24 -0700

    Quick release - patch

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -3,87 +3,86 @@ export default {
     es2022: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@microsoft/sdl/common', // Microsoft SDL rules
-    'plugin:@microsoft/sdl/typescript', // Microsoft SDL TS rules
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:unicorn/recommended',
-    'plugin:stylelint',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@microsoft/sdl",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:stylelint",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: [],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2022, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
   },
   plugins: [
-    '@microsoft/sdl',
-    'functional',
-    'unicorn',
-    'prettier',
-    'only-warn',
-    'simple-import-sort',
-    'sort-keys-fix',
-    '@typescript-eslint',
+    "@microsoft/sdl",
+    "functional",
+    "unicorn",
+    "prettier",
+    "only-warn",
+    "simple-import-sort",
+    "sort-keys-fix",
+    "@typescript-eslint",
   ],
   rules: {
-    '@typescript-eslint/explicit-member-accessibility': ['error'],
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/explicit-member-accessibility": ["error"],
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        format: ['camelCase'],
-        leadingUnderscore: 'allow',
-        selector: 'default',
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+        selector: "default",
       },
       {
-        format: ['camelCase', 'PascalCase', 'snake_case'],
-        selector: 'property',
+        format: ["camelCase", "PascalCase", "snake_case"],
+        selector: "property",
       },
       {
-        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
-        leadingUnderscore: 'allow',
-        selector: 'variable',
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+        leadingUnderscore: "allow",
+        selector: "variable",
       },
-      { format: ['PascalCase'], selector: 'typeLike' },
-      { format: ['PascalCase'], selector: 'enumMember' },
+      { format: ["PascalCase"], selector: "typeLike" },
+      { format: ["PascalCase"], selector: "enumMember" },
     ],
-    '@typescript-eslint/no-shadow': ['error'],
-    'functional/prefer-readonly-type': [
-      'error',
+    "@typescript-eslint/no-shadow": ["error"],
+    "functional/prefer-readonly-type": [
+      "error",
       {
         allowLocalMutation: true,
-        ignorePattern: '^mutable',
+        ignorePattern: "^mutable",
       },
     ],
-    'lines-between-class-members': 0,
-    'max-len': ['error', { code: 120 }],
-    'no-confusing-arrow': ['error', { allowParens: true }],
-    'no-console': 'warn',
-    'no-multiple-empty-lines': 2,
-    'node/no-unsupported-features/es-syntax': [
-      'error',
-      { ignores: ['modules'] },
+    "lines-between-class-members": 0,
+    "max-len": ["error", { code: 120 }],
+    "no-confusing-arrow": ["error", { allowParens: true }],
+    "no-console": "warn",
+    "no-multiple-empty-lines": 2,
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
     ],
-    'object-shorthand': 2,
-    'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': 'error',
-    'sort-imports': 'off',
-    'sort-keys': 2,
-    'sort-keys-fix/sort-keys-fix': 'error',
-    'unicorn/no-null': 'off',
-    'unicorn/no-reduce': 'off',
-    'unicorn/number-literal-case': 'off',
-    'unicorn/prefer-spread': 'off',
-    'unicorn/prevent-abbreviations': 'off',
+    "object-shorthand": 2,
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
+    "sort-imports": "off",
+    "sort-keys": 2,
+    "sort-keys-fix/sort-keys-fix": "error",
+    "unicorn/no-null": "off",
+    "unicorn/no-reduce": "off",
+    "unicorn/number-literal-case": "off",
+    "unicorn/prefer-spread": "off",
+    "unicorn/prevent-abbreviations": "off",
   },
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         // Always try to resolve types under `<root>@types` directory
         // even it doesn't contain any source code, like `@types/unist`
